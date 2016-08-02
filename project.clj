d69c4a10b3b86d4ee52edc9b9877d8f58fe98c5f
Carlos Morgado <chbm@primatas.org>
2016-08-02 10:36:35 +0100

    Bump clojure alpha

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [reduce-fsm "0.1.4"]
                  [org.clojure/core.async "0.2.385"]]
   :main ^:skip-aot pixelsquiz.core
