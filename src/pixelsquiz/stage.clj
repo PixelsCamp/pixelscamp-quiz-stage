96f450644d24cebf7875c71ad2f01b8e4fd7f338
Carlos Morgado <chbm@primatas.org>
2016-08-07 00:14:07 +0100

    Move to http-kit for ws
    sente was a bit too complex regarding clients, http-kit seems to work
    out of the box

--- src/pixelsquiz/stage.clj
+++ src/pixelsquiz/stage.clj
@@ -1,19 +1,16 @@
 (ns pixelsquiz.stage
   (:gen-class)
 
+  (:use org.httpkit.server)
+
   (:require
     [pixelsquiz.types]
     [ring.middleware.defaults]
     [compojure.core     :as comp :refer (defroutes GET POST)]
     [compojure.route    :as route]
-    [taoensso.encore    :as encore :refer (have have?)]
-    [taoensso.timbre    :as timbre :refer (tracef debugf infof warnf errorf)]
-    [taoensso.sente     :as sente]
-    [org.httpkit.server :as http-kit]
-    [taoensso.sente.server-adapters.http-kit :refer (get-sch-adapter)]
     [clojure.core.async :as async :refer [>! <! >!! <!! go go-loop chan buffer close! thread
-             alts! alts!! timeout]])
-     )
+                                          alts! alts!! timeout]])
+  )
 
 (import pixelsquiz.types.Event)
 
@@ -30,20 +27,40 @@
 
 (defn main-display-actor
   []
-  (let [main-display-channel (chan 16)]
+  (let [ws-connections (atom {})
+        main-display-channel (chan 16)]
     (go-loop [ev {:kind :starting}]
-            (println (str "      main-display: " ev))                 
-            (recur (<! main-display-channel)))
+             (println (str "      main-display: " ev))
+             (doseq [client (keys @ws-connections)]
+               ;; send all, client will filter them
+               (send! client ev))            
+             (recur (<! main-display-channel)))
     {:actor :main-display
      :chan main-display-channel
-     :routes nil}))
+     :routes (GET "/maindisplay" req 
+                  (with-channel req channel              ; get the channel
+                    ;; communicate with client using method defined above
+                    (on-close channel (fn [status]
+                                        (swap! ws-connections dissoc channel)
+                                        (println "channel closed")))
+                    (if (websocket? channel)
+                      (do (println "channel conn") (swap! ws-connections assoc channel true))
+                      (println "HTTP channel"))
+                    (on-receive channel (fn [data]       
+                                          ; data received from client
+                                          ;; An optional param can pass to send!: close-after-send?
+                                          ;; When unspecified, `close-after-send?` defaults to true for HTTP channels
+                                          ;; and false for WebSocket.  (send! channel data close-after-send?)
+                                          (send! channel data)))) ; data is sent directly to the client 
+                  )
+     }))
 
 (defn player-lights-actor
   []
   (let [player-lights-channel (chan 16)]
     (go-loop [ev {:kind :starting}]
-            (println "        player-lights: " ev)                 
-            (recur (<! player-lights-channel)))
+             (println "        player-lights: " ev)                 
+             (recur (<! player-lights-channel)))
     {:actor :player-lights
      :chan player-lights-channel
      :routes nil}))
@@ -51,19 +68,21 @@
 (defn quizmaster-actor
   []
   (let [quizmaster-channel (chan 16)]
-      {:actor :quizmaster
-       :chan quizmaster-channel
-       :routes (POST "/actions/:action" [action] 
-                     (>!! quizmaster-channel (Event. (keyword action) {}))
-                      (str "ok " (keyword action) "\n"))
-                     }
-      ))
+    {:actor :quizmaster
+     :chan quizmaster-channel
+     :routes (POST "/actions/:action" [action] 
+                   (>!! quizmaster-channel (Event. (keyword action) {}))
+                   (str "ok " (keyword action) "\n"))
+     }
+    ))
 
 (defn setup-stage
   []
-  (let [actors [(buttons-actor) (main-display-actor) (player-lights-actor) (quizmaster-actor)]
-;        ui-routes (map #(:routes %) actors)]
-      ui-routes (:routes (nth actors 3))]
-    (http-kit/run-server (comp/routes ui-routes) {:port 3000})
-      (apply merge (map #(assoc {} (:actor %) (:chan %)) actors))
+  (let [
+        actors [(buttons-actor) (main-display-actor) (player-lights-actor) (quizmaster-actor)]
+        ui-routes [(:routes (nth actors 3))
+                   (:routes (nth actors 1))]
+        ]
+    (run-server (apply comp/routes ui-routes) {:port 3000})
+    (apply merge (map #(assoc {} (:actor %) (:chan %)) actors))
     ))
