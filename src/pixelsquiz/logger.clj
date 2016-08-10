5e02800ac03d7e94b7ab11007bb4b397f8ebbd34
Carlos Morgado <chbm@primatas.org>
2016-08-10 23:50:48 +0100

    Add a simple logger

---
+++ src/pixelsquiz/logger.clj
@@ -0,0 +1,25 @@
+(ns pixelsquiz.logger
+  (:require
+    [clj-time.core :as t]
+    [clj-time.coerce :as tc]
+    ) 
+  )
+
+(defn print-with-serverity
+  [severity args]
+  (println severity  (.toString (t/now))  args)
+  true)
+
+(defn info
+  [& args]
+  (print-with-serverity "INFO" args )
+  )
+
+(defn error
+  [& args]
+  (print-with-serverity "ERROR" args))
+
+(defn warn
+  [& args]
+  (print-with-serverity "WARN" args))
+
