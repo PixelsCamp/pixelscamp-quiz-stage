74eb09d9b77ddbadc22605f5889e629c6acecbda
Carlos Rodrigues <cer@brpx.com>
2017-12-29 15:18:38 +0000

    Clean up project and increment some dependencies

--- project.clj
+++ project.clj
@@ -1,27 +1,25 @@
-(defproject pixelsquiz "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [spyscope "0.1.5"]
-                  [org.clojars.gjahad/debug-repl "0.3.3"]
-                 [reduce-fsm "0.1.4"]
-                 [org.clojure/core.async "0.2.385"]
-								 [http-kit                             "2.2.0"] ; Default
-								 [ring                      "1.5.0"]
-								 [ring/ring-defaults        "0.2.1"] ; Includes `ring-anti-forgery`, etc.
-								 [compojure                 "1.5.1"] ; Or routing lib of your choice
-                 [org.clojars.torbjornvatn/hidapi "1.1"]
-                 [clj-time "0.12.0"]
-                 [cheshire "5.6.3"]
-                 [org.craigandera/dynne "0.4.1"]
-								 ]
+(defproject pixelsquiz "0.2.0"
+  :description "Game engine for the Pixels Camp quiz"
+  :url "https://github.com/brpx/pixelscamp-quiz-stage"
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/core.async "0.3.465"]
+    [org.clojure/tools.nrepl "0.2.13"]
+    [org.clojure/tools.namespace "0.2.11"]
+    [spyscope "0.1.5"]
+    [org.clojars.gjahad/debug-repl "0.3.3"]
+    [reduce-fsm "0.1.4"]
+    [http-kit "2.2.0"] ; Default
+    [ring "1.5.1"]
+    [ring/ring-defaults "0.3.1"] ; Includes `ring-anti-forgery`, etc.
+    [compojure "1.5.2"] ; Or routing lib of your choice
+    [org.clojars.torbjornvatn/hidapi "1.1"]
+    [clj-time "0.12.2"]
+    [cheshire "5.6.3"]
+    [org.craigandera/dynne "0.4.1"]
+  ]
   :main ^:skip-aot pixelsquiz.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :dev {
-                   :source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
+  :profiles {:uberjar {:aot :all}}
   :jvm-opts ["--add-modules" "java.xml.bind" "-Djava.awt.headless=true"]
   )
