8854c1d7c3f1af6dce82d5dd11b483196c31612e
Carlos Rodrigues <cer@brpx.com>
2018-01-13 17:21:39 +0000

    Do not pop an alert when a team buzzes
    Chrome cancels the alert when switching tabs. Having the buttons
    always available in the console works around this, and is good
    to get out of trouble.

--- html/pixelsquiz/js/quizconsole.js
+++ html/pixelsquiz/js/quizconsole.js
@@ -7,21 +7,26 @@ function send_command(ev) {
     _send_command($(ev.target).attr('id'));
 }
 
-function sendmanual() {
-    _send_command($('#servercmd').val());
-}
-
 $("#commands button").each(function (idx) {
-    $(this).click(send_command)
+    $(this).click(send_command);
+})
+
+$("#buzzed button").each(function (idx) {
+    $(this).click(function (e) {
+        _send_command($(e.target).attr('id'));
+        $("#buzzed .buzzed-text").removeClass("text-highlight");
+    });
 })
 
 function get_right_wrong(team) {
-    var r = window.confirm('Team #'+ (team+1) +' got it right?');
+    /*var r = window.confirm('Team #'+ (team+1) +' got it right?');
     if (r) {
         _send_command('select-right');
     } else {
         _send_command('select-wrong');
-    }
+    }*/
+    $("#buzzed .buzzed-team").text(team + 1);
+    $("#buzzed .buzzed-text").addClass("text-highlight");
 }
 
 var ws = null;
@@ -43,7 +48,7 @@ function start() {
             }
         } else {
             if (msg.do === 'update-all' || msg.do === 'update-scores' || msg.do === 'update-lights') {
-                $('#status').text(JSON.stringify(msg) + '\n' + $('#status').text());
+                $('#status').append(JSON.stringify(msg) + '\n');
             }
         }
     }
