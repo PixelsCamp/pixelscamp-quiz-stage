f7499d5f7609c91eb46d2d4ade48e03a8c318224
Carlos Rodrigues <cer@brpx.com>
2019-05-04 19:27:54 +0100

    Allow advancing the game with a clicker

--- html/pixelsquiz/js/quizconsole.js
+++ html/pixelsquiz/js/quizconsole.js
@@ -76,5 +76,75 @@ function check() {
 }
 
 check();
-
 setInterval(check, 3000);
+
+
+var last_clicker_press = 0;
+var min_clicker_interval = 2000;  // milliseconds
+
+/*
+ * If you paid attention to the quiz flowchart, you may have realized that only one
+ * command button is active at a time. Actually, there are multiple buttons so that
+ * the quizmaster doesn't lose track of the game state.
+ *
+ * This means we can use a wireless clicker to advance the game if we want to. :)
+ *
+ * We might also want to use the clicker to accept/reject an buzzed answer, but that
+ * may be dangerous if the quizmaster confuses the buttons, so that's not implemented.
+ */
+$(document).keyup(function(e) {
+    if (!e.charCode && (e.which === 33 || e.which === 34)) {  // "page up" and "page down".
+        e.preventDefault();
+
+        var now = new Date().getTime();
+        var click_elapsed = now - last_clicker_press;
+        last_clicker_press = now;
+
+        // Protect against successive clicks (debounce)...
+        if (click_elapsed < min_clicker_interval) {
+            console.warn('not enough time elapsed between click sequences: ' + click_elapsed + 'ms');
+            return;
+        }
+
+        /*
+         * The show question button is clicked twice to show the question immediately.
+         * The quizmaster may not be in front of the console to read it, after all.
+         *
+         * The start/finish round button isn't included, just because.
+         */
+        var buttons = [
+            'start-question',
+            'show-question', 'show-question',
+            'start-mult'
+        ];
+
+        console.log('simulating (synchronous) click sequence to advance game');
+
+        for (var i = 0; i < buttons.length; i++) {
+            var button = buttons[i];
+
+            $('#' + button).click();
+            synchronous_sleep(20);
+        }
+    }
+});
+
+
+/*
+ * We use "keyup" above to simulate a complete press of "page up" and "page down".
+ * This means we need to prevent scrolling from happening on the "keydown" event.
+ */
+$(document).keydown(function(e) {
+    if (!e.charCode && (e.which === 33 || e.which === 34)) {  // "page up" and "page down".
+        e.preventDefault();
+    }
+});
+
+function synchronous_sleep(ms) {
+    var start = new Date().getTime();
+    var expire = start + ms;
+
+    while (new Date().getTime() < expire) {}
+
+    return;
+}
